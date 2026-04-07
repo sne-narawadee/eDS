b8ad1fd75b22c403b50bf223214e1f42286aaf90
sne-narawadee <166264806+sne-narawadee@users.noreply.github.com>
2026-04-07 21:18:23 +0700

    Update sw.js

--- APPS/eDS/sw.js
+++ APPS/eDS/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eDS-v2';
+const CACHE = 'eDS-v3';
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
