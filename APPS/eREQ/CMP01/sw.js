1dc67405d36870e39ba1b5333e55530efda800e9
sne-narawadee <166264806+sne-narawadee@users.noreply.github.com>
2026-04-09 21:58:16 +0700

    Create sw.js

---
+++ APPS/eREQ/CMP01/sw.js
@@ -0,0 +1,32 @@
+/* if update code into index.html then change version this code */
+
+const CACHE = 'eREQ-v1';
+/*-------------------------------------------------------------*/
+const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith('http')) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type !== 'opaque') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => e.request.mode === 'navigate' ? caches.match('./index.html') : undefined);
+    })
+  );
+});
