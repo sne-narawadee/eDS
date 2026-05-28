b3e0869e20495420e4868b3a95202ac4dee17438
sne-narawadee <166264806+sne-narawadee@users.noreply.github.com>
2026-05-28 17:31:21 +0700

    Update sw.js

--- APPS/eDS/sw.js
+++ APPS/eDS/sw.js
@@ -1,6 +1,6 @@
 /* if update code into index.html then change version this code */
 
-const CACHE = 'eDS-v4';
+const CACHE = 'eDS-v5';
 /*-------------------------------------------------------------*/
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
